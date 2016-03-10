55cc99adcdfeff12d583020c6f7b27ff1bf8bd3d
DeanLym <yiminliu@cees-rcf-tool-4.stanford.edu>
2016-03-10 13:40:49 -0800

    Romove files

--- Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lnomadmpi -lsimlym -lmkl_intel_lp64 -lmkl_core -lmkl_sequential -lpthread -lopca -llapack -lblas -lf2c -ltmg -lnlopt -lm -lmpi
-
