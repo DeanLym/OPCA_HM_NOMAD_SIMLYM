1eb1babba9f9fe82adbaec90e7d77c6b18d25ae6
DeanLym <yiminliu@cees-rcf-tool-4.stanford.edu>
2016-03-18 15:06:26 -0700

    prepare files for model1_opca

--- sim_model.hpp
+++ sim_model.hpp
@@ -11,15 +11,16 @@
 #include "CSimCtrl.h"
 
 SimCtrl* GetSimulationModel(double *kx){
+	// Model 1 Simulator file
 	SimCtrl* sim;
 	sim = new SimCtrl;
 	sim->display_level_ = 1;
 	// input and initialize grid
 	sim->grid_ = new CCartGrid(60,60,1);
-	sim->grid_->InputDx(82.02);
-	sim->grid_->InputDy(82.02);
+	sim->grid_->InputDx(164.042);
+	sim->grid_->InputDy(164.042);
 	sim->grid_->InputDz(32.81);
-	sim->grid_->InputTops(11466.54);
+	sim->grid_->InputTops(25574.15);
 	sim->grid_->InputKx(kx);
 	sim->grid_->InputKy(kx);
 	sim->grid_->InputKz(kx);
@@ -34,60 +35,146 @@ SimCtrl* GetSimulationModel(double *kx){
 	sim->pvt_->set_pvtw_table("PVTW.DAT");
 
 	// initialize SAT
-	sim->sat_ = new CSAT_COREY(0.1, 0.2 ,2.0 ,2.0 ,0.5 ,0.9 );
+	//	sim->sat_ = new CSAT_COREY(0.1, 0.2 ,2.0 ,2.0 ,0.5 ,0.9 );
+	sim->sat_ = new CSAT_TABLE;
+	sim->sat_->SetSWOF(16,"SCAL.DAT");
 
 	// initialize SCH
 	sim->sch_ = new CSchedule;
 	sim->sch_->SetTEnd(600.0);
 	sim->sch_->SetTCurrent(0.0);
 	sim->sch_->SetTNext(sim->sch_->GetDt() + sim->sch_->GetTCurrent());
-	sim->sch_->SetdTmax(30.0);
+	sim->sch_->SetdTmax(100.0);
 	vector<double> report_time;
-	report_time.push_back(0.5);
 	report_time.push_back(100.0); report_time.push_back(200.0);report_time.push_back(300.0);
 	report_time.push_back(400.0); report_time.push_back(500.0);report_time.push_back(600.0);
-	sim->sch_->SetReportTime(7, &report_time[0]);
+	sim->sch_->SetReportTime(6, &report_time[0]);
 
 	// initialize State
 	sim->InitializeState();
-	sim->SetInitPres(11482.94,5076.33);
+	sim->SetInitPres(25590.55,4713.735);
 	// Set initial saturation equal to swi
 	sim->SetInitSat(0.1, 0.0);
 
 	// initialize Well
-	CStandardWell *p_1 = new CSTDProdWell("PROD-1", sim->grid_->GetIndex(4,19,0));
-	p_1->set_r(1);
+	double bhp_ = 2175.57;
+	CStandardWell *p_1 = new CSTDProdWell("PROD-1", sim->grid_->GetIndex(12,6,0));
+	p_1->set_r(0.5);
 	p_1->set_ctrl_mode(CStandardWell::CBHP);
-	p_1->set_TL_BHP(4931.3);
+	p_1->set_TL_BHP(bhp_);
 	p_1->CalWellIndex(sim->grid_);
 	sim->std_well_.push_back(p_1);
-	CStandardWell *p_2 = new CSTDProdWell("PROD-2", sim->grid_->GetIndex(9,9,0));
-	p_2->set_r(1);
+
+	CStandardWell *p_2 = new CSTDProdWell("PROD-2", sim->grid_->GetIndex(12,20,0));
+	p_2->set_r(0.5);
 	p_2->set_ctrl_mode(CStandardWell::CBHP);
-	p_2->set_TL_BHP(4931.3);
+	p_2->set_TL_BHP(bhp_);
 	p_2->CalWellIndex(sim->grid_);
 	sim->std_well_.push_back(p_2);
-	CStandardWell *p_3 = new CSTDProdWell("PROD-3", sim->grid_->GetIndex(54,54,0));
-	p_3->set_r(1);
+
+	CStandardWell *p_3 = new CSTDProdWell("PROD-3", sim->grid_->GetIndex(12,32,0));
+	p_3->set_r(0.5);
 	p_3->set_ctrl_mode(CStandardWell::CBHP);
-	p_3->set_TL_BHP(4931.3);
+	p_3->set_TL_BHP(bhp_);
 	p_3->CalWellIndex(sim->grid_);
 	sim->std_well_.push_back(p_3);
+
+	CStandardWell *p_4 = new CSTDProdWell("PROD-4", sim->grid_->GetIndex(12,50,0));
+	p_4->set_r(0.5);
+	p_4->set_ctrl_mode(CStandardWell::CBHP);
+	p_4->set_TL_BHP(bhp_);
+	p_4->CalWellIndex(sim->grid_);
+	sim->std_well_.push_back(p_4);
+
+	CStandardWell *p_5 = new CSTDProdWell("PROD-5", sim->grid_->GetIndex(24,6,0));
+	p_5->set_r(0.5);
+	p_5->set_ctrl_mode(CStandardWell::CBHP);
+	p_5->set_TL_BHP(bhp_);
+	p_5->CalWellIndex(sim->grid_);
+	sim->std_well_.push_back(p_5);
+
+	CStandardWell *p_6 = new CSTDProdWell("PROD-6", sim->grid_->GetIndex(24,35,0));
+	p_6->set_r(0.5);
+	p_6->set_ctrl_mode(CStandardWell::CBHP);
+	p_6->set_TL_BHP(bhp_);
+	p_6->CalWellIndex(sim->grid_);
+	sim->std_well_.push_back(p_6);
+
+	CStandardWell *p_7 = new CSTDProdWell("PROD-7", sim->grid_->GetIndex(40,6,0));
+	p_7->set_r(0.5);
+	p_7->set_ctrl_mode(CStandardWell::CBHP);
+	p_7->set_TL_BHP(bhp_);
+	p_7->CalWellIndex(sim->grid_);
+	sim->std_well_.push_back(p_7);
+
+	CStandardWell *p_8 = new CSTDProdWell("PROD-8", sim->grid_->GetIndex(40,20,0));
+	p_8->set_r(0.5);
+	p_8->set_ctrl_mode(CStandardWell::CBHP);
+	p_8->set_TL_BHP(bhp_);
+	p_8->CalWellIndex(sim->grid_);
+	sim->std_well_.push_back(p_8);
+
+	CStandardWell *p_9 = new CSTDProdWell("PROD-9", sim->grid_->GetIndex(40,35,0));
+	p_9->set_r(0.5);
+	p_9->set_ctrl_mode(CStandardWell::CBHP);
+	p_9->set_TL_BHP(bhp_);
+	p_9->CalWellIndex(sim->grid_);
+	sim->std_well_.push_back(p_9);
+
+	CStandardWell *p_10 = new CSTDProdWell("PROD-10", sim->grid_->GetIndex(40,52,0));
+	p_10->set_r(0.5);
+	p_10->set_ctrl_mode(CStandardWell::CBHP);
+	p_10->set_TL_BHP(bhp_);
+	p_10->CalWellIndex(sim->grid_);
+	sim->std_well_.push_back(p_10);
+
+	CStandardWell *p_11 = new CSTDProdWell("PROD-11", sim->grid_->GetIndex(52,6,0));
+	p_11->set_r(0.5);
+	p_11->set_ctrl_mode(CStandardWell::CBHP);
+	p_11->set_TL_BHP(bhp_);
+	p_11->CalWellIndex(sim->grid_);
+	sim->std_well_.push_back(p_11);
+
+	CStandardWell *p_12 = new CSTDProdWell("PROD-12", sim->grid_->GetIndex(52,45,0));
+	p_12->set_r(0.5);
+	p_12->set_ctrl_mode(CStandardWell::CBHP);
+	p_12->set_TL_BHP(bhp_);
+	p_12->CalWellIndex(sim->grid_);
+	sim->std_well_.push_back(p_12);
+
 	//=====================================
-	CStandardWell *inj_1 = new CSTDWInjWell("INJ-1", sim->grid_->GetIndex(29,24, 0));
-	inj_1->set_r(1);
-	inj_1->set_ctrl_mode(CStandardWell::CBHP);
-	inj_1->set_TL_BHP(5148.8);
+	double wrat_target_inj = -9447.0, bhp_limit_inj = 14503.8;
+	CStandardWell *inj_1 = new CSTDWInjWell("INJ-1", sim->grid_->GetIndex(24, 20, 0));
+	inj_1->set_r(0.5);
+	inj_1->set_ctrl_mode(CStandardWell::CWRAT);
+	inj_1->set_TL_WRAT(wrat_target_inj);
+	inj_1->set_TL_BHP(bhp_limit_inj);
 	inj_1->CalWellIndex(sim->grid_);
 	sim->std_well_.push_back(inj_1);
 
-	CStandardWell *inj_2 = new CSTDWInjWell("INJ-2", sim->grid_->GetIndex(29,44, 0));
-	inj_2->set_r(1);
-	inj_2->set_ctrl_mode(CStandardWell::CBHP);
-	inj_2->set_TL_BHP(5148.8);
+	CStandardWell *inj_2 = new CSTDWInjWell("INJ-2", sim->grid_->GetIndex(24, 52, 0));
+	inj_2->set_r(0.5);
+	inj_2->set_ctrl_mode(CStandardWell::CWRAT);
+	inj_2->set_TL_WRAT(wrat_target_inj);
+	inj_2->set_TL_BHP(bhp_limit_inj);
 	inj_2->CalWellIndex(sim->grid_);
 	sim->std_well_.push_back(inj_2);
 
+	CStandardWell *inj_3 = new CSTDWInjWell("INJ-3", sim->grid_->GetIndex(52, 25, 0));
+	inj_3->set_r(0.5);
+	inj_3->set_ctrl_mode(CStandardWell::CWRAT);
+	inj_3->set_TL_WRAT(wrat_target_inj);
+	inj_3->set_TL_BHP(bhp_limit_inj);
+	inj_3->CalWellIndex(sim->grid_);
+	sim->std_well_.push_back(inj_3);
+
+	CStandardWell *inj_4 = new CSTDWInjWell("INJ-4", sim->grid_->GetIndex(52, 56, 0));
+	inj_4->set_r(0.5);
+	inj_4->set_ctrl_mode(CStandardWell::CWRAT);
+	inj_4->set_TL_WRAT(wrat_target_inj);
+	inj_4->set_TL_BHP(bhp_limit_inj);
+	inj_4->CalWellIndex(sim->grid_);
+	sim->std_well_.push_back(inj_4);
 	// initialize Solver
 	sim->InitializeSolver();
 	return sim;
